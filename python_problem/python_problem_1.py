1470d1a9f7262c2e6536470b30bd95411c29ed54
devkev00 <kimminsoo116@gmail.com>
2024-07-05 08:20:42 +0900

    8단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,38 +1,29 @@
+def brGame():
+    while True:
+        try:
+            count = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : '))
+            if count in [1, 2, 3]:
+                return count
+            else:
+                print("1, 2, 3 중 하나를 입력하세요.")
+        except ValueError:
+            print("정수를 입력하세요.")
+
 num = 0
 
-while num <= 31: 
-  while True:
-    try: 
-      count_A = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : '))
-      if count_A in [1, 2, 3]:
+while num <= 31:
+    count_A = brGame()
+    for i in range(1, count_A + 1):
+        num += 1
+        print(f"playerA : {num}")
+    if num >= 31:
+        print("PlayerB win!")
         break
-      else:
-        print("1, 2, 3 중 하나를 입력하세요.")
-    except ValueError:
-      print("정수를 입력하세요.")
 
-  for i in range(1, count_A + 1):
-    num += 1
-    print(f"playerA : {num}")
-
-  if num >= 31:
-    print("PlayerB win!")
-    break
-
-  while True:
-    try: 
-      count_B = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : '))
-      if count_B in [1, 2, 3]:
+    count_B = brGame()
+    for i in range(1, count_B + 1):
+        num += 1
+        print(f"playerB : {num}")
+    if num >= 31:
+        print("PlayerA win!")
         break
-      else:
-        print("1, 2, 3 중 하나를 입력하세요.")
-    except ValueError:
-      print("정수를 입력하세요.")
-
-  for i in range(1, count_B + 1):
-    num += 1
-    print(f"playerB : {num}")
-
-  if num >= 31:
-    print("PlayerA win!")
-    break
